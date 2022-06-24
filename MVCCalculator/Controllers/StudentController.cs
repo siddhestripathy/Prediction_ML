ead96612d5e4fef7e99641c96ac0b070a15adc09
siddhestripathy <siddhestripathy@gmail.com>
2022-06-24 23:42:22 +0530

    initial

---
+++ MVCCalculator/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCalculator.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Student()
+        {
+            return View();
+        }
+    }
+}
--- MVCCalculator/Program.cs
+++ MVCCalculator/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Student}/{action=Student}/{id?}");
 
 app.Run();
